799e33302884a11f22fe8a094af42c3e9d1d7ea1
Akash Vishwa <akashav1617@gmail.com>
2023-12-06 22:41:41 +0530

    new project update

--- src/Components/Cards/WordCard.jsx
+++ src/Components/Cards/WordCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./WordCardStyle.css"
-import pimg from '../../assets/movieapp.png'
+import Mimg from '../../assets/movieapp.png'
+import Simg from '../../assets/searchapp.png'
 import { NavLink } from 'react-router-dom';
 
 const WordCard = () => {
@@ -9,44 +10,43 @@ const WordCard = () => {
       <h1 className="aboutHead">PROJECT</h1>
       <div className="projCon">
         <div className="projCard">
-          <img src={pimg} alt="no-image" />
+          <img src={Mimg} alt="no-image" />
           <h2 className="projT">Movies App</h2>
-          <div className='projDel'>
-          <p>
-          "Explore and discover movies effortlessly with our Movie Reviews App. Get insightful reviews and information on a wide range of films for an enhanced cinematic experience."
-          </p>
-          <div className="projBtns">
-          <NavLink to="https://av-movies-app.vercel.app/~" className="btn">View</NavLink>  
-          </div>
+          <div className="projDel">
+            <p>
+              "Explore and discover movies effortlessly with our Movie Reviews
+              App. Get insightful reviews and information on a wide range of
+              films for an enhanced cinematic experience."
+            </p>
+            <div className="projBtns">
+              <NavLink to="https://av-movies-app.vercel.app/~" className="btn">
+                View
+              </NavLink>
+            </div>
           </div>
         </div>
 
-     <div className="projCard">
-     <img src={pimg} alt="no-image" />
-     <h2 className="projT">Movies App</h2>
-     <div className='projDel'>
-     <p>
-     dummy
-     </p>
-     <div className="projBtns">
-     <NavLink to="https://av-movies-app.vercel.app/~" className="btn">View</NavLink>  
-     </div>
-     </div>
-     </div>
+        <div className="projCard">
+          <img src={Simg} alt="no-image" />
+          <h2 className="projT">Image Search App</h2>
+          <div className="projDel">
+            <p>
+              "Unleash the power of visual search with Image Seeker, your go-to
+              app for effortlessly finding information through images. Get
+              instant and relevant results within seconds."
+            </p>
+            <div className="projBtns">
+              <NavLink
+                to="https://av-image-search-app-rosy.vercel.app/"
+                className="btn"
+              >
+                View
+              </NavLink>
+            </div>
+          </div>
 
-     <div className="projCard">
-     <img src={pimg} alt="no-image" />
-     <h2 className="projT">Movies App</h2>
-     <div className='projDel'>
-     <p>
-     dummy
-     </p>
-     <div className="projBtns">
-     <NavLink to="https://av-movies-app.vercel.app/~" className="btn">View</NavLink>  
-     </div>
-     </div>
-     </div>
-       
+          
+        </div>
       </div>
     </div>
   );
@@ -55,21 +55,10 @@ const WordCard = () => {
 export default WordCard
 
 
+
 // <div className="projCard">
 // <img src={pimg} alt="no-image" />
-// <h2 className="projT">Movies App</h2>
-// <div className='projDel'>
-// <p>
-// dummy
-// </p>
-// <div className="projBtns">
-// <NavLink to="https://av-movies-app.vercel.app/~" className="btn">View</NavLink>  
-// </div>
-// </div>
-// </div>
-// <div className="projCard">
-// <img src={pimg} alt="no-image" />
-// <h2 className="projT">Movies App</h2>
+// <h2 className="projT">App</h2>
 // <div className='projDel'>
 // <p>
 // dummy
